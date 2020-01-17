2e2d45d74cf5f98c55ce95c8de7ee2303f5655dd
unknown <15097@alunos.ipbeja.pt>
2020-01-17 16:17:41 +0000

    Finished parsing the logs, finished the data for the ssh logfile, added functions to retrieve data, created charts with matplotlib for better understanding

--- projectFiles/DatabaseHandler.py
+++ projectFiles/DatabaseHandler.py
@@ -4,20 +4,39 @@ import sqlite3
 class DatabaseHandler:
     def __init__(self):
         self.connection = sqlite3.connect("Connection_attempts.db")
+        self.createDatabase()
 
     def createDatabase(self):
-        print("Creating Table Attampts")
+        print("Creating Table Attempts")
         sqlQuery = """CREATE TABLE IF NOT EXISTS Attempts (
                     id integer PRIMARY KEY AUTOINCREMENT,
-                    IP text unique not null,
+                    IP text not null,
                     Time text not null,
-                    Location text not null
+                    Location text not null,
+                    Message text 
                 )"""
         self.connection.execute(sqlQuery)
 
     def insertData(self, data):
-        print("Hello world")
+        print(data)
+        sql = """INSERT INTO Attempts(Time,IP,Location,Message)
+              VALUES(?,?,?,?)"""
+        cur = self.connection.cursor()
+        cur.execute(sql, data)
+        self.connection.commit()
 
+    def selectAll(self):
+        cur = self.connection.cursor()
+        cur.execute("SELECT * FROM Attempts")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-dbh = DatabaseHandler()
-dbh.createDatabase()
+    def selectCount(self):
+        cur = self.connection.cursor()
+        cur.execute(
+            "SELECT Location, COUNT(*) FROM Attempts GROUP BY Location ORDER BY COUNT(*) ASC"
+        )
+        rows = cur.fetchall()
+        return rows
+
--- projectFiles/logReader.py
+++ projectFiles/logReader.py
@@ -1,8 +1,14 @@
 import re
 import geoip2.database
+from DatabaseHandler import DatabaseHandler
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Reader:
+    def __init__(self):
+        self.dbh = DatabaseHandler()
+
     def find_str(self, s, char):
         index = 0
 
@@ -19,40 +25,117 @@ class Reader:
 
     # Function that will get the country of origin of the ip
     def getCountry(self, ip):
-        reader = geoip2.database.Reader("projectFiles/GeoLite2-Country.mmdb")
+        try:
+            reader = geoip2.database.Reader("../GeoLiteFiles/GeoLite2-Country.mmdb")
+            returnData = reader.country(ip)
+            return returnData.country.name
+        except:
+            print("An exception occured")
 
-        returnData = reader.country(ip)
-        return returnData.country.name
+    def logTypeSelector(self):
 
-    # This functio will parse the log files, returning the ips and the corresponding location
+        LogType = input(
+            "What kind of log do you want to parse ?\n 1 - SSH \n 2 - UFW \n"
+        )
 
-    def ip_parser(self):
-        file = open("projectFiles/auth.log")
+        if LogType == 1:
+            filePath = raw_input(
+                "File path(This must be the absolute filepath in order to work properly):"
+            )
+            try:
+                self.sshParser(filePath)
+            except:
+                print("We Couldn't find the file you specified!")
+                self.logTypeSelector()
+        elif LogType == 2:
+            print("Some stuff is hardcoded")
+        else:
+            self.logTypeSelector()
+
+    def ufwParser(self):
+        file = open("../logFiles/auth.log")
+        file = file.readlines()
+
+    # This function will parse the SSH log file, returning the ips and the corresponding location
+    def sshParser(self, filepath):
+        print(filepath)
+        # file = open("../logFiles/auth.log")
+        file = open(filepath)
         file = file.readlines()
         ips = []
-        dates = []
         for line in file:
-            # ips_temp = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", line)
             if re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", line):
-                print(
-                    "Connection attempt from {}".format(
-                        self.getCountry(
-                            re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", line)[
-                                0
-                            ]
+                ipFound = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", line)[0]
+                message = ""
+                ipLocation = self.getCountry(ipFound)
+                if ipFound not in "0.0.0.0":
+                    if ipFound not in ips:
+                        ips.append(ipFound)
+                    print(
+                        "Date: {} Ip: {} From {}".format(
+                            line[0:16], ipFound, ipLocation
                         )
                     )
-                )
-                # print(line)
-                # dates.append(line)
-                # for d in dates:
-                # dates_temp = re.findall(r"\b\d{1,2}\:\d{1,2}\:\d{1,2}\b", d)[0]
-                # print(dates_temp)
-                """ for ip in ips_temp:
-                    if ip not in ips:
-                        ips.append(ip)
-                        print(ip) """
+                    if "pam_unix(sshd:auth)" in line:
+                        searchKeyword = "pam_unix(sshd:auth)"
+                        message = line[
+                            line.find(searchKeyword)
+                            + len(searchKeyword)
+                            + 2 : line.find(";")
+                        ]
+                    else:
+                        message = line.split(":")[3]
+
+                    if ipLocation is None:
+                        self.dbh.insertData(
+                            [line[0:15], ipFound, "Nao encontrada", message]
+                        )
+                    else:
+                        self.dbh.insertData([line[0:14], ipFound, ipLocation, message])
+
+    def CreatePiePlot(self):
+        self.CountList = self.dbh.selectCount()
+        labels = []
+        sizes = []
+        for row in self.CountList:
+            print(row)
+            labels.append(row[0])
+            sizes.append(row[1])
+
+        x = labels
+        y = sizes
+        colors = [
+            "yellowgreen",
+            "red",
+            "gold",
+            "lightskyblue",
+            "lightcoral",
+            "darkgreen",
+            "blue",
+            "pink",
+            "yellow",
+            "grey",
+            "violet",
+            "magenta",
+            "cyan",
+        ]
+
+        patches, texts = plt.pie(y, colors=colors, startangle=90, radius=1.2)
+        labels = ["{0} - {1}".format(i, j) for i, j in zip(x, y)]
+
+        sort_legend = True
+        if sort_legend:
+            patches, labels, dummy = zip(
+                *sorted(zip(patches, labels, y), key=lambda x: x[2], reverse=True)
+            )
+
+        plt.legend(
+            patches, labels, loc="center left", bbox_to_anchor=(-0.1, 1.0), fontsize=8
+        )
+
+        plt.savefig("Authentication Attempts.png", bbox_inches="tight")
+        # plt.show()
 
 
 tester = Reader()
-tester.ip_parser()
+tester.logTypeSelector()
